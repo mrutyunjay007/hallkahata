88b9997220625142cca78e4998969420e5df2f9f
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-04-26 14:18:10 +0530

    Add tokens in cookies

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -16,90 +16,112 @@ import Link from "next/link";
 import { userPassword, userPhoneNumber } from "@/schema/userSchema";
 import Image from "next/image";
 import india from "@/../public/india.png";
+import { IoIosArrowRoundForward } from "react-icons/io";
+import { toast } from "@/components/ui/use-toast";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 function Login() {
   const [password, setPassword] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
 
-  const [passwordError, setPasswordError] = useState("");
-  const [phoneNumberError, setPhoneNumberError] = useState("");
+  const route = useRouter();
 
   return (
-    <div className="w-full h-screen flex justify-center items-center px-5">
-      <div>
-        <Card className="w-full md:w-[350px]">
-          <CardHeader>
-            <CardTitle>Login</CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-col gap-3">
-            <span>
-              <div className="flex justify-center items-center gap-2 ">
-                <div className="h-9 w-1/2  rounded-lg border-2 border-zinc-300 flex justify-center items-center gap-2 ">
-                  <Image className="size-5" src={india} alt="" />
-                  <span>+91</span>
-                </div>
-                <Input
-                  placeholder="phone number"
-                  className=" "
-                  onChange={(e) => {
-                    e?.preventDefault();
-                    setPhoneNumber(e.target.value);
-                  }}
-                ></Input>
-              </div>
-              {phoneNumberError.length > 0 && (
-                <span className="text-sm font-mono text-red-500 text-muted-foreground">
-                  {phoneNumberError}
-                </span>
-              )}
-            </span>
-
-            <span>
-              <Label htmlFor="password">Password:</Label>
-              <Input
-                type="password"
-                id="password"
-                placeholder="password"
-                onChange={(e) => {
-                  e?.preventDefault();
-                  setPassword(e.target.value);
-                }}
-              ></Input>
-              {passwordError.length > 0 && (
-                <span className="text-sm font-mono text-red-500 text-muted-foreground">
-                  {passwordError}
-                </span>
-              )}
-            </span>
-          </CardContent>
-          <CardFooter className="w-full flex justify-end">
-            {/* <Link href="/signup/addphonenumber"> */}
-            <Button
-              className="bg-sky-900"
-              onClick={() => {
-                const validatePhoneNumber =
-                  userPhoneNumber.safeParse(phoneNumber);
-
-                !validatePhoneNumber.success
-                  ? setPhoneNumberError(
-                      validatePhoneNumber.error.errors[0].message
-                    )
-                  : setPhoneNumberError("");
-
-                const validatePassword = userPassword.safeParse(password);
-                !validatePassword.success
-                  ? setPasswordError(validatePassword.error.errors[0].message)
-                  : setPasswordError("");
+    <div className=" w-full flex flex-col justify-center items-center h-screen bg-[#ffc300] ">
+      <div className="w-full p-10 flex flex-col gap-5 justify-center items-center">
+        <div className="w-full text-5xl mb-5 font-bold text-primary">
+          <span>Welcome!</span>
+          {/* <div className="text-primary text-sm">keep every thing in track</div> */}
+        </div>
+        <div className=" w-full flex flex-col justify-center items-center gap-5  ">
+          <div className=" w-full flex justify-center items-center gap-2  ">
+            <div className="h-9 w-1/2 py-8 rounded-lg  bg-white flex justify-center items-center gap-2  ">
+              <Image className="size-5" src={india} alt="" />
+              <span className="bg-white">+91</span>
+            </div>
+            <Input
+              placeholder="phone number"
+              className=" border-none px-4 py-8"
+              onChange={(e) => {
+                e?.preventDefault();
+                setPhoneNumber(e.target.value);
               }}
-            >
-              Next
-            </Button>
-            {/* </Link> */}
-          </CardFooter>
-        </Card>
-        <div className="w-full text-center mt-2 font-mono text-sm text-muted-foreground">
-          {" "}
-          Create a new account
+            ></Input>
+          </div>
+
+          <Input
+            type="password"
+            id="password"
+            className=" py-8 px-5 border-none"
+            placeholder="password"
+            onChange={(e) => {
+              e?.preventDefault();
+              setPassword(e.target.value);
+            }}
+          ></Input>
+        </div>
+      </div>
+      <div className="w-full h-32 relative flex justify-start items-center  px-11">
+        <Link href={"/signup"}>
+          <div className="  text-center font-bold text-lg text-primary ">
+            To create a new account
+            <div className="text-start cursor-pointer">{" Signup"}</div>
+          </div>
+        </Link>
+
+        {/* next btn */}
+        <div
+          className="w-16 h-32 flex justify-center items-center top-0 right-0 drop-shadow-md absolute bg-primary rounded-l-full cursor-pointer"
+          onClick={() => {
+            const validatePhoneNumber = userPhoneNumber.safeParse(phoneNumber);
+
+            !validatePhoneNumber.success &&
+              toast({
+                variant: "destructive",
+                title: validatePhoneNumber.error?.errors[0].message,
+              });
+
+            const validatePassword = userPassword.safeParse(password);
+            !validatePassword.success &&
+              validatePhoneNumber.success &&
+              toast({
+                variant: "destructive",
+                title: validatePassword.error?.errors[0].message,
+              });
+
+            if (validatePassword.success && validatePhoneNumber.success) {
+              (async () => {
+                try {
+                  const { data } = await axios.post(
+                    "http://localhost:3000/api/login",
+                    {
+                      phoneNumber,
+                      password,
+                    },
+                    {
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                    }
+                  );
+
+                  if (data.success) {
+                    route.push("/customers");
+                  }
+                } catch (error: any) {
+                  if (error.response.status === 401) {
+                    toast({
+                      variant: "destructive",
+                      title: "number or password is incorrect",
+                    });
+                  }
+                }
+              })();
+            }
+          }}
+        >
+          <IoIosArrowRoundForward className="size-7 text-white " />
         </div>
       </div>
     </div>
--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,6 +2,8 @@ import dbConnection from "@/lib/dbConnect";
 import UserModel from "@/models/User";
 import bcrypt from "bcryptjs";
 import Response from "@/util/Response";
+import jwt from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
 
 // Login controler
 export async function POST(request: Request) {
@@ -12,20 +14,52 @@ export async function POST(request: Request) {
     const { phoneNumber, password } = await request.json();
 
     //get user
-    const user = await UserModel.findOne({ phoneNumber });
+    const user = await UserModel.aggregate([
+      {
+        $match: {
+          phoneNumber,
+        },
+      },
+    ]);
 
-    if (!user) {
+    if (!user[0]) {
       return Response(false, "Unauthorized!", 401);
     }
 
     //match password
-    const isPasswordMatched = bcrypt.compareSync(password, user.password);
+    const isPasswordMatched = bcrypt.compareSync(password, user[0].password);
 
     if (!isPasswordMatched) {
       return Response(false, "Unauthorized!", 401);
     }
 
-    return Response(true, "Login successfull!", 200);
+    //create token data
+    const tokenData = {
+      id: user[0]._id,
+      username: user[0].username,
+      phoneNumber: user[0].phoneNumber,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json(
+      {
+        success: true,
+        message: "Login successfull!",
+      },
+      {
+        status: 200,
+      }
+    );
+
+    // add token in cookies
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
+    return response;
   } catch (error) {
     console.log(error);
     return Response(false, "Login failed!", 500);
