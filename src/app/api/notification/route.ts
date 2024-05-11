d500b3f9e686b3a6bf6b39fafd9e0abd5509c762
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-05-11 19:18:16 +0530

    update notification handelling in backend

--- src/app/api/notification/route.ts
+++ src/app/api/notification/route.ts
@@ -1,5 +1,7 @@
+import Connection from "@/components/Connection";
 import dbConnection from "@/lib/dbConnect";
 import BillModel from "@/models/Bill";
+import ConnectionModel from "@/models/Connection";
 import Response, { ResponseServerError } from "@/util/Response";
 import mongoose from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
@@ -13,22 +15,20 @@ export async function GET(request: Request) {
     //   const bill = url.searchParams.get("bill");
 
     const billData = await BillModel.aggregate([
-      // get all bills of same customer with no aprooval or with remainder
+      // get all bills of same customer with no aprooval or same seller with paid bill and no aprooval
       {
         $match: {
           $or: [
             {
+              // me as customer notification receved from seller to aproove
               customerNumber: "8777761381",
-              $or: [
-                { aprooved: false },
-                {
-                  $and: [{ aprooved: true }, { remainder: true }],
-                },
-              ],
+              aprooved: false,
             },
+            // me as seller notification receved from customer of paid bill
             {
               sellerNumber: "8777761380",
               paid: true,
+              aprooved: false,
             },
           ],
         },
@@ -58,6 +58,31 @@ export async function GET(request: Request) {
           },
         },
       },
+      // lookup to get customer data
+      {
+        $lookup: {
+          from: "users",
+          localField: "customerNumber",
+          foreignField: "phoneNumber",
+          as: "customer",
+          pipeline: [
+            {
+              $project: {
+                userName: 1,
+                phoneNumber: 1,
+              },
+            },
+          ],
+        },
+      },
+      {
+        // flatten array
+        $addFields: {
+          customer: {
+            $arrayElemAt: ["$customer", 0],
+          },
+        },
+      },
 
       {
         $project: {
@@ -65,23 +90,65 @@ export async function GET(request: Request) {
           seller: 1,
           customer: 1,
           aprooved: 1,
-          remainder: 1,
           amount: 1,
           createdAt: 1,
           paid: 1,
-          paymentProcess: 1,
+          paymentType: 1,
         },
       },
     ]);
 
-    if (billData.length === 0) {
+    // me as customer get all bills of same customer with remainder
+    const remainderData = await ConnectionModel.aggregate([
+      {
+        $match: {
+          customerNumber: "8777761381",
+          remainder: true,
+        },
+      },
+      // get seller data
+      {
+        $lookup: {
+          from: "users",
+          localField: "sellerNumber",
+          foreignField: "phoneNumber",
+          as: "seller",
+          pipeline: [
+            {
+              $project: {
+                userName: 1,
+                phoneNumber: 1,
+              },
+            },
+          ],
+        },
+      },
+      {
+        // flatten array
+        $addFields: {
+          seller: {
+            $arrayElemAt: ["$seller", 0],
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          customerNumber: 1,
+          seller: 1,
+          amount: 1,
+        },
+      },
+    ]);
+
+    if (billData.length === 0 && remainderData.length === 0) {
       return Response(false, "no such bill present", 404);
     }
 
     return NextResponse.json(
       {
         success: true,
-        data: billData,
+        data: [...billData, ...remainderData],
         message: "bill got successfully!",
       },
       {
@@ -97,13 +164,13 @@ export async function POST(request: NextRequest) {
   //db connected
   await dbConnection();
   try {
-    const { billId, type } = await request.json();
-    console.log(billId);
+    //id can be bill id or connection id
+    const { id, type } = await request.json();
 
     const bill = await BillModel.aggregate([
       {
         $match: {
-          _id: new mongoose.Types.ObjectId(billId!),
+          _id: new mongoose.Types.ObjectId(id!),
         },
       },
     ]);
@@ -114,11 +181,11 @@ export async function POST(request: NextRequest) {
 
     // if notification type is aprooval -> make aproove true
     // if notification type is remainder -> make remainder false
-    // if notification type is payment -> delete bill and put in history
+    // if notification type is payment ->  make aproove true
 
     if (type === "aprooval") {
       await BillModel.updateOne(
-        { _id: billId },
+        { _id: id },
         {
           aprooved: true,
         }
@@ -126,13 +193,10 @@ export async function POST(request: NextRequest) {
 
       return Response(true, "bill aprooved successfully!", 200);
     } else if (type === "remainder") {
-      await BillModel.findOneAndUpdate(billId, { remainder: false });
+      await ConnectionModel.findOneAndUpdate(id, { remainder: false });
       return Response(true, "remainder updated successfully!", 200);
     } else {
-      await BillModel.findByIdAndDelete(billId);
-      //TODO:
-      //create new bill in history
-      // update connection amount
+      await BillModel.updateOne({ _id: id }, { aprooved: true });
     }
   } catch (error: any) {
     console.log(error.message);
