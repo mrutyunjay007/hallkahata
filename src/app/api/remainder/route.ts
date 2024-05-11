a022075cd145bc3eebd8238526d1d796ca5b6321
Mrutyunjay <mrutyunjaydhal07@gmail.com>
2024-05-11 19:29:07 +0530

    update remainder

--- src/app/api/remainder/route.ts
+++ src/app/api/remainder/route.ts
@@ -1,5 +1,5 @@
 import dbConnection from "@/lib/dbConnect";
-import BillModel from "@/models/Bill";
+import ConnectionModel from "@/models/Connection";
 import Response, { ResponseServerError } from "@/util/Response";
 
 export async function POST(request: Request) {
@@ -7,22 +7,22 @@ export async function POST(request: Request) {
     //db connected
     await dbConnection();
 
-    const { billId } = await request.json();
+    const { connectionId } = await request.json();
 
-    // update bill remainder to true
-    const bill = await BillModel.updateOne(
-      { _id: billId },
+    // update remainder to true
+    const remainderNotification = await ConnectionModel.updateOne(
+      { _id: connectionId },
       {
         remainder: true,
       }
     );
 
-    if (!bill.acknowledged) {
-      return Response(false, "bill not found!", 404);
+    if (!remainderNotification.acknowledged) {
+      return Response(false, "connection not found!", 404);
     }
 
-    return Response(true, "bill updated successfully!", 200);
+    return Response(true, "remainder susessfully send!", 200);
   } catch (error) {
-    return ResponseServerError("bill update failed!");
+    return ResponseServerError("remainder update failed!");
   }
 }
